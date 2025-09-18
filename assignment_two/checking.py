f0907586a6451fb884a11811fee09ac8c30bcd4c
Raymundo <raymundomc0205@icloud.com>
2025-09-17 23:19:25 -0400

    small remove

--- assignment_two/checking.py
+++ assignment_two/checking.py
@@ -7,14 +7,14 @@ class CheckingAcc(RayBank):
 
     def withdraw(self, amount):
         if self.current_balance - amount < -self.overdraft_limit:
-            print(f"❌ Withdrawal denied. You’ve exceeded your overdraft limit of {self.overdraft_limit}.")
+            print(f" Withdrawal denied. You’ve exceeded your overdraft limit of {self.overdraft_limit}.")
         else:
             self.current_balance -= amount
-            print(f"✅ Withdrawal successful. Current balance: {self.current_balance}")
+            print(f" Withdrawal successful. Current balance: {self.current_balance}")
 
     def apply_fee(self, fee=25):
         if self.current_balance < self.minimum_balance:
             self.current_balance -= fee
-            print(f"⚠️ Fee applied! ${fee} deducted. New balance: {self.current_balance}")
+            print(f" Fee applied! ${fee} deducted. New balance: {self.current_balance}")
         else:
-            print("✅ No fees applied, balance is healthy.")
+            print(" No fees applied, balance is healthy.")
