a16451a5cddcda970869dc533693ca455e37a60f
Raymundo <raymundomc0205@icloud.com>
2025-10-20 02:19:12 -0400

    added Item class

--- fast_api_implementation/main.py
+++ fast_api_implementation/main.py
@@ -5,6 +5,12 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+    
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
